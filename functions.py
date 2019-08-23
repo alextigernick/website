e9651d18116a8d9eab7560dcf40f903773def606
Alex Jones <alextigernick@gmail.com>
2019-08-23 13:03:09 -0500

    Initial stuff for Radio Lab

--- functions.py
+++ functions.py
@@ -3,6 +3,7 @@ import os,inspect
 def list_dir(x):
     output = ""
     for each in os.listdir(site_root+x):
+        
         if os.path.isdir(os.path.join(site_root+x, each)):
             output += "<a href='{}/'/><p>{}</p></a>".format(os.path.join(x,each),each)
     return output
