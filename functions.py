ae6fd7536c6b88b03f5ccc92b56cebc221627c27
Alex Jones <alextigernick@gmail.com>
2019-10-05 22:38:22 -0500

    Lab 3 - yeehaw

--- functions.py
+++ functions.py
@@ -2,8 +2,7 @@ import os,inspect
 #site_root = os.path.dirname(os.path.abspath(inspect.stack()[0][1]))
 def list_dir(x):
     output = ""
-    for each in os.listdir(site_root+x):
-        
+    for each in sorted(os.listdir(site_root+x)):
         if os.path.isdir(os.path.join(site_root+x, each)):
             output += "<a href='{}/'/><p>{}</p></a>".format(os.path.join(x,each),each)
     return output
