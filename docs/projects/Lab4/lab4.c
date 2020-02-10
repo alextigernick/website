3d849fe25ec1f39e337da80162bb015988e5134e
Alex Jones <alextigernick@gmail.com>
2020-02-10 17:03:13 -0600

    corrected lab4 code

--- docs/projects/Lab4/lab4.c
+++ docs/projects/Lab4/lab4.c
@@ -4,30 +4,36 @@
 #define SW2 GPIOA->IDR & 0x04
 
 uint8_t count = 0;
-uint8_t count2 = 9;
-int dir = 1
+uint8_t count2 = 0;
+uint8_t ledState1 = 0;
+uint8_t ledState2 = 0;
+int dir = 1;
 void EXTI0_IRQHandler(){
-	EXTI_PR = 0x00FFFFFF;	
+	EXTI->PR = 0x00FFFFFF;
+	ledState1 = !ledState1;
+	d();
 	dir = -1;
 }
 void EXTI1_IRQHandler(){
-	EXTI_PR = 0x00FFFFFF;
+	EXTI->PR = 0x00FFFFFF;
+	ledState2 = !ledState2;
+	d();
 	dir = 1;
 }
 void pinSetup() {
     RCC->APB1ENR |= 0x00000010;
     RCC->AHBENR  |= 0x05;
-    GPIOA->MODER &= ~0x0000003C;
-    GPIOC->MODER &= ~0x0000FFFF;
-    GPIOC->MODER |=  0x00005555;
+    GPIOA->MODER &= 0xFFFF0000;
+    GPIOC->MODER &= ~0xFFFFFFFF;
+    GPIOC->MODER |=  0x55555555;
 }
-void count(uint8_t *count,int dir){
+void countF(uint8_t *count,int dir){
     *count += dir;
     if (*count == 255) {
-        count = 9;
+        *count = 9;
     }
     else if (*count == 10) {
-        count = 0;
+        *count = 0;
     }
 }
 void delay(){
@@ -38,21 +44,29 @@ void delay(){
     TIM6->CR1 |= TIM_CR1_CEN;
     while (!(TIM6->SR & TIM_SR_UIF));
 }
+void d(){
+GPIOC->ODR = ((ledState2&0x01) << 9)|((ledState1&0x01) << 8)|((count2 & 0x0F) << 4) | (count & 0x0F);
+}
+	
 int main(void){
-    pinSetup();
-	SYSCFG_EXTICR1 = 0;
-	EXTI_RTSR = 0x00FFFFFF;
-	EXTI_EMR = 	0x00000003; 
-	//NVIC_EnableIRQ(EXTI0_IRQn);
-	//NVIC_EnableIRQ(EXTI1_IRQn);
+    
+		SYSCFG->EXTICR[0] = 0;
+		EXTI->RTSR = 0x00FFFFFF;
+		EXTI->EMR = 	0x00000003; 
+	  EXTI->IMR = 	0x00000003; 
+	  NVIC_EnableIRQ(EXTI0_IRQn);
+	  NVIC_EnableIRQ(EXTI1_IRQn);
+	  NVIC_ClearPendingIRQ(EXTI0_IRQn);  
+	  NVIC_ClearPendingIRQ(EXTI1_IRQn);  
+		pinSetup();
+	  __enable_irq();
     while(1) {
-        if (SW1) {
-            delay();
-            count(&count,1);
-            delay();
-            count(&count,1);
-            count(&count2,dir);
-        }
-        GPIOC->ODR = ((count2 & 0x0F) << 4) || (count & 0x0F);
+				delay();
+				countF(&count,1);
+        d();
+				delay();
+				countF(&count,1);
+				countF(&count2,dir);
+        d();
     }
 }
